ea149eada75ed314f43b37a29892a0b4f0854027
Sylvain Gaunet <3924333-sgaunet@users.noreply.gitlab.com>
2021-09-22 07:33:47 +0200

    Handle parameter to specify one tag

--- src/go.sh
+++ src/go.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-ansible-playbook -i hosts installation.yml -l portable --ask-become-pass
+tag="$1"
+
+if [ -z "$tag" ]
+then
+    ansible-playbook -i hosts installation.yml -l portable --ask-become-pass
+else
+    ansible-playbook -i hosts installation.yml -l portable --ask-become-pass -t $tag
+fi
