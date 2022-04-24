3b32037d13a4dff8f4f18d7ce5425827486784a9
Sylvain Gaunet <1552102+sgaunet@users.noreply.github.com>
2022-04-24 21:51:43 +0200

    Add script gitst.sh

---
+++ src/roles/gitconfig/files/gitst.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+find . -name .git | while read ligne
+do 
+    repo=$(echo "$ligne" | sed "s#\.git##g")
+    cd "$repo"
+        st=$(git status -s)
+        if [ "$st" != "" ]
+        then
+            echo "$repo"
+        fi
+    cd - > /dev/null 2>&1
+done
